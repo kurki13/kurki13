d6e4f64395aebdab786df56f640b171b0f857fe9
Mikko Tamminen <mikko.k.tamminen@helsinki.fi>
2013-09-18 02:40:08 +0300

    Vielä pieni korjaus skriptiin

--- install_project.sh
+++ install_project.sh
@@ -4,7 +4,8 @@ wanna-tomcat
 stop-tomcat
 git clone https://github.com/kurki13/kurki13.git
 cd kurki13
-git pull origin master
+git fetch --all
+git reset --hard origin/master
 mvn package
 mv ~/kurki13/target/kurki13-1.0-SNAPSHOT.war ~/tomcat/webapps
 mv ~/kurki13/apache-tomcat-7.0.42/conf/tomcat-users.xml ~/tomcat/conf/tomcat-users.xml
