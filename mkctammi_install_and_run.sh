524f1e2ee7d91a9d297e5fe8177379e67701874f
Mikko Tamminen <pahanaama@gmail.com>
2013-09-19 15:51:54 +0300

    users -asennusskripti toimimaan kaikille

--- mkctammi_install_and_run.sh
+++ mkctammi_install_and_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mvn package
-scp target/kurki13-1.0-SNAPSHOT.war mkctammi@users.cs.helsinki.fi:/home/mkctammi/tomcat/webapps/
+scp -i id_rsa target/kurki13-1.0-SNAPSHOT.war mkctammi@users.cs.helsinki.fi:/home/mkctammi/tomcat/webapps/
 chromium-browser http://t-mkctammi.users.cs.helsinki.fi/kurki13-1.0-SNAPSHOT/servlet/index
