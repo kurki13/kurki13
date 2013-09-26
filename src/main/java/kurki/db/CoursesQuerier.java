0f3638ed34efdfb48192353dd6115d7f72c16095
Mikko Tamminen <pahanaama@gmail.com>
2013-09-26 13:26:08 +0300

    Separated SQL from Session

--- src/main/java/kurki/db/CoursesQuerier.java
+++ src/main/java/kurki/db/CoursesQuerier.java
@@ -1,36 +1,29 @@
-package debug;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kurki.db;
 
-
-import kurki.Rooli;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import kurki.Rooli;
 import kurki.model.CourseInfo;
-import kurki.db.DBConnectionManager;
-import service.ServiceManager;
 import service.exception.NullIdException;
-import service.exception.ServicesNotLockedException;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author mkctammi
  */
-public class Pipe {
+public class CoursesQuerier {
 
-    public static final int MONTHS_OPEN = 12;
-    public static final int SUPER_OPEN = 48;
-    
-    protected static final String COURSE_INFOS =
-            //<editor-fold defaultstate="collapsed" desc="courseInfos">
+    //Kuinka monta kuukautta kursseja saa muokata
+    private static final int MONTHS_OPEN = 12;
+    private static final int SUPER_OPEN = 48;
+    private static final String COURSE_INFOS = //<editor-fold defaultstate="collapsed" desc="courseInfos">
 
             // LUENTO- (JA LABORATORIOKURSSIT)
             "SELECT DISTINCT k.kurssikoodi, k.lukuvuosi, k.lukukausi, k.tyyppi, k.tila,\n"
@@ -72,8 +65,7 @@ public class Pipe {
             + "    AND h.ktunnus = ?)\n"
             + "ORDER BY orderBy ASC, nimi ASC, alkamis_pvm ASC";
     //</editor-fold>
-    protected static final String SUPER_INFOS =
-            //<editor-fold defaultstate="collapsed" desc="superInfos">
+    private static final String SUPER_INFOS = //<editor-fold defaultstate="collapsed" desc="superInfos">
 
             "SELECT DISTINCT k.kurssikoodi, k.lukuvuosi, k.lukukausi, k.tyyppi, k.tila,\n"
             + "    k.kurssi_nro, k.nimi AS nimi, k.alkamis_pvm AS alkamis_pvm, k.paattymis_pvm,\n"
@@ -81,8 +73,11 @@ public class Pipe {
             + "  FROM kurssi k\n"
             + "  WHERE k.paattymis_pvm > to_date('31.12.2001','DD.MM.YYYY')\n"
             + "    AND ADD_MONTHS( k.paattymis_pvm, " + SUPER_OPEN + " ) >= SYSDATE\n"
+            //	+"    AND k.alkamis_pvm <= SYSDATE+30\n"
+            //      +"    AND k.tyyppi in ('K', 'S')\n" // vain luentokurssit ja seminaarit
             + "    AND k.tyyppi in ('K', 'S', 'A')\n" // luento- ja laboratoriokurssit sekä seminaarit
             + "    AND k.tila not in ('S', 'O')\n"
+            // 	+"    AND k.tila not in ('S', 'O', 'J')\n" // ei jäädytettyjä kursseja
 
             // KOKEET
             + "UNION\n"
@@ -92,70 +87,67 @@ public class Pipe {
             + "  FROM kurssi ku\n"
             + "  WHERE ku.paattymis_pvm > to_date('31.12.2001','DD.MM.YYYY')\n"
             + "    AND ADD_MONTHS( ku.paattymis_pvm, " + SUPER_OPEN + " ) >= SYSDATE\n"
+            //	+"    AND ku.paattymis_pvm <= SYSDATE+30\n"
             + "    AND ku.tila not in ('S', 'O')\n"
+            // 	+"    AND ku.tila not in ('S', 'O', 'J')\n" // ei jäädytettyjä kursseja
             + "    AND ku.tyyppi = 'L')\n" // vain kokeet
             + "ORDER BY orderBy ASC, nimi ASC, alkamis_pvm ASC";
     //</editor-fold>
+    public static class CourseQueryResult {
+        public HashMap<String, CourseInfo> coursesMap;
+        public CourseInfo[] coursesList;
 
-    public static String getName() {
-        return "Not a pipe";
+        public CourseQueryResult(HashMap<String, CourseInfo> coursesMap, CourseInfo[] coursesList) {
+            this.coursesMap = coursesMap;
+            this.coursesList = coursesList;
+        }
+        
     }
 
-    public static String course_infos() {
-        HashMap<String, CourseInfo> courses = new HashMap();
+    public static CourseQueryResult super_infos() throws SQLException, ClassNotFoundException, NullIdException {
+        Connection databaseConnection = DBConnectionManager.createConnection();
+        PreparedStatement preparedStatement = databaseConnection.prepareStatement(SUPER_INFOS);
+        return execute_statement(databaseConnection, preparedStatement);
+    }
+
+    public static CourseQueryResult course_infos(String ruser) throws SQLException, ClassNotFoundException, NullIdException {
+        Connection databaseConnection = DBConnectionManager.createConnection();
+        PreparedStatement preparedStatement = databaseConnection.prepareStatement(COURSE_INFOS);
+        preparedStatement.setString(1, ruser);
+        preparedStatement.setString(2, ruser);
+        return execute_statement(databaseConnection, preparedStatement);
+    }
+
+    private static CourseQueryResult execute_statement(Connection databaseConnection, PreparedStatement preparedStatement) throws NullIdException, SQLException {
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        HashMap<String, CourseInfo> coursesMap = new HashMap();
         ArrayList<CourseInfo> coursesList = new ArrayList();
-        String answer = "";
-        
-        PreparedStatement preparedStatement;
-        try {
-            Connection databaseConnection = DBConnectionManager.createConnection();
-            preparedStatement = databaseConnection.prepareStatement(SUPER_INFOS);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            
-            while (resultSet.next()) {
-		int lukuvuosi = resultSet.getInt("lukuvuosi");
-		String lukukausi = resultSet.getString("lukukausi");
-		String tila = resultSet.getString("tila");
-                String tyyppi = resultSet.getString("tyyppi");
-                int kurssiNumero = resultSet.getInt("kurssi_nro");
-                String nimi = resultSet.getString("nimi");
-		String nameaux  = " ["+lukukausi+(""+lukuvuosi).substring(2, 4)+"]";
 
-		CourseInfo cinfo = new CourseInfo( resultSet.getString("kurssikoodi"),
-					lukuvuosi,
-					lukukausi,
-					tyyppi,
-					kurssiNumero,
-					nimi+nameaux,
-					Rooli.SUPER );
-                if (tila != null && tila.equals("J")) 
-                    cinfo.freeze();
-		courses.put( cinfo.getId(), cinfo );
-		coursesList.add( cinfo );
-                
-                answer += cinfo.toString() + "<br>";
-                DBConnectionManager.closeConnection(databaseConnection);
+        while (resultSet.next()) {
+            int lukuvuosi = resultSet.getInt("lukuvuosi");
+            String lukukausi = resultSet.getString("lukukausi");
+            String tila = resultSet.getString("tila");
+            String nameaux = " [" + lukukausi + ("" + lukuvuosi).substring(2, 4) + "]";
+
+            CourseInfo cinfo = new CourseInfo(resultSet.getString("kurssikoodi"),
+                    lukuvuosi,
+                    lukukausi,
+                    resultSet.getString("tyyppi"),
+                    resultSet.getInt("kurssi_nro"),
+                    resultSet.getString("nimi") + nameaux,
+                    Rooli.SUPER); // kurssin perustiedot
+            if (tila != null && tila.equals("J")) {
+                cinfo.freeze();
             }
-        } catch (SQLException ex) {
-            Logger.getLogger(Pipe.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        catch (ClassNotFoundException ex) {
-            Logger.getLogger(Pipe.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NullIdException ex) {
-            Logger.getLogger(Pipe.class.getName()).log(Level.SEVERE, null, ex);
+            coursesMap.put(cinfo.getId(), cinfo);
+            coursesList.add(cinfo);
         }
 
+        resultSet.close();
+        preparedStatement.close();
         
-        return answer;
-    }
-    
-    public static String service_manager() {
-        ServiceManager service = null;
-        try {
-            service = ServiceManager.getInstance();
-        } catch (ServicesNotLockedException ex) {
-            Logger.getLogger(Pipe.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return service.toString();
+        DBConnectionManager.closeConnection(databaseConnection);
+        return new CourseQueryResult(coursesMap, coursesList.toArray(new CourseInfo[coursesList.size()]));
     }
 }
