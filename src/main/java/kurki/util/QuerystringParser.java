b42b9a754d4e897dc8ed48f6e6251e0cbe1a9531
Mikko Tamminen <mikko.k.tamminen@helsinki.fi>
2013-10-04 00:07:22 +0300

    Get parameters finally working

--- src/main/java/kurki/util/QuerystringParser.java
+++ src/main/java/kurki/util/QuerystringParser.java
@@ -5,6 +5,7 @@
 package kurki.util;
 
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.util.LinkedHashMap;
@@ -15,19 +16,13 @@ import java.util.Map;
  * @author mkctammi
  */
 public class QuerystringParser {
-
-    public static Map<String, String> splitQuery(URL url) throws UnsupportedEncodingException {
+    public static Map<String,String> split(String query) throws UnsupportedEncodingException, MalformedURLException {
         Map<String, String> query_pairs = new LinkedHashMap<String, String>();
-        String query = url.getQuery();
         String[] pairs = query.split("&");
         for (String pair : pairs) {
             int idx = pair.indexOf("=");
             query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
         }
         return query_pairs;
-    }
-    
-    public String lol() {
-        return "lol";
     }
 }
