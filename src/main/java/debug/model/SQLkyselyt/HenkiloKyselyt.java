f294b799c1d8c44340feede4c3438ffa4a36b977
Taru A <taru.airola@helsinki.fi>
2013-10-07 15:56:30 +0300

    SQL-kyselyiden paketti lisätty

---
+++ src/main/java/debug/model/SQLkyselyt/HenkiloKyselyt.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package debug.model.SQLkyselyt;
+
+/**
+ *
+ * @author tkairola
+ */
+public class HenkiloKyselyt {
+    public static final String kyselynnimi = "";
+}
