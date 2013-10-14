cea8d73299f949d81e41a9bed8cae95a80b39685
Mikko Tamminen <pahanaama@gmail.com>
2013-10-14 14:02:35 +0300

    Moved files around

--- src/main/java/debug/util/QuerystringParser.java
+++ src/main/java/debug/util/QuerystringParser.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package kurki.util;
+package debug.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
