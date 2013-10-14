bd4c2226a435030274a4368171c94bbe0f99ba64
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2013-10-14 14:08:06 +0300

    Pohja testaukselle jwebunitilla

---
+++ src/test/java/testitesti.java
@@ -0,0 +1,37 @@
+
+import net.sourceforge.jwebunit.junit.*;
+import net.sourceforge.jwebunit.util.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testitesti {
+
+    private WebTester tester;
+    private TestContext context;
+
+    @Before
+    public void prepare() {
+        tester = new WebTester();
+        tester.setBaseUrl("http://localhost:8080/kurki13");
+
+        context = new TestContext();
+        context.setAuthorization("admin", "admin");
+
+        tester.setTestContext(context);
+    }
+
+    @Test
+    public void title() {
+        tester.beginAt("kurki13/hello.vm");
+        tester.assertTitleEquals("KURKI");
+    }
+
+    @Test
+    public void nappulat() {
+        tester.beginAt("kurki13/hello.vm");
+        tester.assertButtonPresentWithText("kurssihaku");
+        tester.assertButtonPresentWithText("laskarit");
+        tester.assertButtonPresentWithText("opiskelija");
+        tester.assertButtonPresentWithText("osallistujaLista");
+    }
+}
