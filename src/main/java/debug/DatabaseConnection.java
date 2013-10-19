36dde87ae80c780f320bfbde9ab1ff8d38bb8515
Mikko Tamminen <mikko.k.tamminen@gmail.com>
2013-10-20 01:32:41 +0300

    Testikanta pois gitistä

--- src/main/java/debug/DatabaseConnection.java
+++ src/main/java/debug/DatabaseConnection.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -31,19 +33,35 @@ public class DatabaseConnection {
             throws SQLException, ClassNotFoundException, IOException {
         String driver = "org.apache.derby.jdbc.EmbeddedDriver";
         Class.forName(driver);
-        File dbFolder = new File(new File("./testDB").getAbsolutePath());
-        String url = "jdbc:derby:" + dbFolder.getCanonicalPath();
+        File dbFolder = new File(new File("./testikanta").getAbsolutePath());
+        String url = "jdbc:derby:" + dbFolder.getCanonicalPath() + ";create=true";
+        System.out.println(url);
         Connection c = DriverManager.getConnection(url);
         return c;
     }
 
-    public static void main(String[] args) throws IOException {
-            try {
-                Connection c = createTestDatabaseConnection();
-            } catch (SQLException ex) {
-                Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
-            }
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        Connection c = createTestDatabaseConnection();
+        String query = "CREATE TABLE Testi\n"
+                + "(\n"
+                + "Numero int,\n"
+                + "Teksti varchar(255)\n"
+                + ")";
+        c.prepareStatement(query).execute();
+        
+        
+        query = "INSERT INTO Testi (Numero,Teksti)\n"
+                + "VALUES (123, 'Iik')\n";
+        c.prepareStatement(query).execute();
+        
+        query = "select * from testi";
+        PreparedStatement ps = c.prepareStatement(query);
+        ps.execute();
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()) {
+            System.out.println(rs.getInt("Numero"));
+            System.out.println(rs.getString("Teksti"));
+        }
+        c.close();
     }
 }
