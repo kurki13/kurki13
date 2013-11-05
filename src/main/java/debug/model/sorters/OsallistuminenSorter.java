49735276e8fd03ef1191f2637be1dae9d929dfbc
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2013-11-05 15:08:21 +0200

    Sorttaaminen käänteisesti ja lista jostain johonkin

--- src/main/java/debug/model/sorters/OsallistuminenSorter.java
+++ src/main/java/debug/model/sorters/OsallistuminenSorter.java
@@ -19,6 +19,10 @@ public class OsallistuminenSorter {
     public static void sorttaaSukunimenMukaan(List<Osallistuminen> lista) {
         Collections.sort(lista, new OsallistuminenSukunimenMukaan());
     }
+    
+     public static void sorttaaSukunimenMukaanKaanteinen(List<Osallistuminen> lista) {
+        Collections.sort(lista, new OsallistuminenSukunimenMukaanKaanteinen());
+    }
 
     public static void sorttaaIlmjarjestyksenMukaan(List<Osallistuminen> lista) {
         Collections.sort(lista, new OsallistuminenIlmjarjestyksenMukaan());
@@ -29,20 +33,72 @@ public class OsallistuminenSorter {
         if (StringUtils.isNumeric(filter)) {
             int numero = Integer.parseInt(filter);
             for (Osallistuminen osallistuminen : lista) {
-                if (osallistuminen.getIlmo_jnro() != numero) {                    
+                if (osallistuminen.getIlmo_jnro() != numero) {
                     kopio.remove(osallistuminen);
                 }
             }
-            lista = kopio; 
-            return lista;
+            return kopio;
+
         } else {
-            for (Osallistuminen osallistuminen : lista) {
-                if (!osallistuminen.getSukunimi().matches(filter + ".*")) {                    
-                    kopio.remove(osallistuminen);
+            if (filter.contains("..")) {
+                System.out.println("Pisteitä filterissä!");
+                System.out.println(filter);
+                Collections.sort(kopio, new OsallistuminenSukunimenMukaan());
+                Collections.sort(lista, new OsallistuminenSukunimenMukaan());
+                String[] sanat = filter.split("\\.\\.");
+                if (sanat.length == 1) {
+                    String alku = sanat[0].trim();
+                    System.out.println(alku);
+                    for (Osallistuminen osallistuminen : lista) {
+                        if (!osallistuminen.getSukunimi().matches(alku + ".*")) {                            
+                            kopio.remove(osallistuminen);
+                        } else {                            
+                            break;
+                        }
+                    }
+                    return kopio;
+                } else if (sanat.length > 1) {
+                    String alku = sanat[0].trim();
+                    String loppu = sanat[1].trim();
+                    System.out.println(alku);
+                    for (Osallistuminen osallistuminen : lista) {
+                        if (!osallistuminen.getSukunimi().matches(alku + ".*")) {                            
+                            kopio.remove(osallistuminen);
+                        } else {                           
+                            break;
+                        }
+                    }  
+                    Collections.sort(lista, new OsallistuminenSukunimenMukaanKaanteinen());
+                    for (Osallistuminen osallistuminen : lista) {
+                        if (!osallistuminen.getSukunimi().matches(loppu + ".*")) {                            
+                            kopio.remove(osallistuminen);
+                        } else {                           
+                            break;
+                        }
+                    }                    
+                    return kopio;
+                } else {
+                    return lista;
                 }
             }
-            lista = kopio; 
-            return lista;
-        }     
+            if (filter.charAt(0) != '#') {
+                for (Osallistuminen osallistuminen : lista) {
+                    if (!osallistuminen.getSukunimi().matches(filter + ".*")) {
+                        kopio.remove(osallistuminen);
+                    }
+                }
+                lista = kopio;
+                return lista;
+            } else if (filter.charAt(0) == '#') {
+                String substring = filter.substring(1);
+                for (Osallistuminen osallistuminen : lista) {
+                    if (!osallistuminen.getHetu().matches(substring + ".*")) {
+                        kopio.remove(osallistuminen);
+                    }
+                }
+                return kopio;
+            }
+        }
+        return lista;
     }
 }
