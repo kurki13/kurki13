0f1ab95205bf959e0470b7a12ef3c154255214b4
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2013-11-05 13:40:08 +0200

    Listojen filteröinti sukunimen ja ryhmänumeron mukaan

--- src/main/java/debug/model/sorters/OsallistuminenSorter.java
+++ src/main/java/debug/model/sorters/OsallistuminenSorter.java
@@ -5,21 +5,44 @@
 package debug.model.sorters;
 
 import debug.model.Osallistuminen;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import org.apache.commons.lang.StringUtils;
 
 /**
  *
  * @author topisark
  */
 public class OsallistuminenSorter {
-    
+
     public static void sorttaaSukunimenMukaan(List<Osallistuminen> lista) {
         Collections.sort(lista, new OsallistuminenSukunimenMukaan());
     }
-    
+
     public static void sorttaaIlmjarjestyksenMukaan(List<Osallistuminen> lista) {
         Collections.sort(lista, new OsallistuminenIlmjarjestyksenMukaan());
     }
-    
+
+    public static List filteroi(List<Osallistuminen> lista, String filter) {
+        ArrayList<Osallistuminen> kopio = new ArrayList<Osallistuminen>(lista);
+        if (StringUtils.isNumeric(filter)) {
+            int numero = Integer.parseInt(filter);
+            for (Osallistuminen osallistuminen : lista) {
+                if (osallistuminen.getIlmo_jnro() != numero) {                    
+                    kopio.remove(osallistuminen);
+                }
+            }
+            lista = kopio; 
+            return lista;
+        } else {
+            for (Osallistuminen osallistuminen : lista) {
+                if (!osallistuminen.getSukunimi().matches(filter + ".*")) {                    
+                    kopio.remove(osallistuminen);
+                }
+            }
+            lista = kopio; 
+            return lista;
+        }     
+    }
 }
