10f1fd2e461320d1f4df17a5aec4515734d2962c
Taru A <taru.airola@helsinki.fi>
2013-11-28 18:13:51 +0200

    filteröintiluokan käyttö osallistujatietojen muutokseen ja listapohjan muokkausta

--- src/main/java/view/util/Filter.java
+++ src/main/java/view/util/Filter.java
@@ -53,14 +53,18 @@ public class Filter {
      * filteröintiparametrien mukaan.
      */
     private static void filteriApuri(HttpServletRequest request, String[] filterit, Osallistuminen os, String ryhma) {
+        System.out.println("testii2"+onkoRyhmassa(ryhma, os));
         int hetu = 0;
         int nimi = 0;
         int joukossa = 0;
+        System.out.println(filterit.toString());
         for (String filter : filterit) {
+            System.out.println("f"+filter);
             if (onkoRyhmassa(ryhma, os)) {
                 if (isInteger(filter)) {
                     if (os.getHetu().toLowerCase().startsWith(filter)) {
                         hetu = 1;
+                        System.out.println("hetu");
                     }
                 } else {
                     if (filter.contains("..")) {
@@ -68,6 +72,7 @@ public class Filter {
                             joukossa = 1;
                         }
                     } else if (os.getSukunimi().toLowerCase().startsWith(filter)) {
+                        System.out.println("nimi");
                         nimi = 1;
                     }
                 }
