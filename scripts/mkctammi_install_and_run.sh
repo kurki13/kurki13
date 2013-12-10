43d68bf95750732f43fc9160570e0799a4b29969
Heikki I Havukainen <heikkiha@dat-9.cs.helsinki.fi>
2013-12-10 11:17:33 +0200

    Siistitty kansiorakennetta

--- scripts/mkctammi_install_and_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-scp -i scripts/id_rsa target/kurki13-1.0-SNAPSHOT.war mkctammi@users.cs.helsinki.fi:/home/mkctammi/tomcat/webapps/
-chromium-browser http://t-mkctammi.users.cs.helsinki.fi/kurki13-1.0-SNAPSHOT/servlet/index
