4757e53c1cfe1e185f4345a50875f91a6d19df5d
Heikki I Havukainen <heikkiha@dat-9.cs.helsinki.fi>
2013-12-10 11:20:57 +0200

    Kansiorakennetta siistitty

--- scripts/mkctammi_rsync_and_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rsync -az --progress ~/github/kurki13 mkctammi@users.cs.helsinki.fi:/home/mkctammi/
-ssh -i id_rsa mkctammi@users.cs.helsinki.fi 'bash -s' < package_and_install.sh
-
--- scripts/package_and_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~/kurki13
-mvn install
-rm -rf ~/tomcat/webapps/kurki13-1.0-SNAPSHOT
-rm -f ~/tomcat/webapps/kurki13-1.0-SNAPSHOT.war
-cp ~/kurki13/target/kurki13-1.0-SNAPSHOT.war ~/tomcat/webapps
-cp ~/kurki13/apache-tomcat-7.0.42/conf/tomcat-users.xml ~/tomcat/conf/tomcat-users.xml
